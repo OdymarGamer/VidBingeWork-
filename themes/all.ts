e09d68aa2373978033709f8e3638e5dc421315f8
OdymarGamer <103310250+OdymarGamer@users.noreply.github.com>
2025-01-17 22:46:17 +0200

    Update all.ts extra themes

--- themes/all.ts
+++ themes/all.ts
@@ -2,10 +2,12 @@ import teal from "./list/teal";
 import red from "./list/red";
 import gray from "./list/gray";
 import blck from "./list/blck";
+import greenpiunk from "./list/greenpiunk";
 
 export const allThemes = [
   teal,
   gray,
   red,
-  blck
-]
+  blck,
+  greenpiunk
+];
